25d7d1e00ddcddd9e42142408bd1a00833f5d7d0
Arnold2801 <avr2801@quicknet.nl>
2026-01-09 14:20:11 +0100

    Boekhouding toegevoegd met presentatie en CSV-export

--- helper.py
+++ helper.py
@@ -1,7 +1,34 @@
 def decoreer(tekst=""):
-    lengte = len(tekst) + 4
+    lengte = len(tekst) + 4   
+    print()    
+    print(lengte * "*")    
+    print(f"* {tekst} *")   
+    print(lengte * "*")    
     print()
-    print(lengte * "*")
-    print(f"* {tekst} *")
-    print(lengte * "*")
-    print()         
+ 
+def fooi_pp(bedrag, personen):
+    bedrag_pp = bedrag/personen
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit 
+
+ijsjes = {
+    "aardbeien": 1000,
+    "vanille": 2000,
+    "chocolade": 1500,
+    "waterijsjes": 750
+}
+
+def totaal_ijsjes(data):
+    return sum(data.values())
+
+totaal = totaal_ijsjes(ijsjes)
+print(totaal)
+
+
+   
+
---
+++ presentatie.py
@@ -0,0 +1,18 @@
+def presenteer(totaal):
+    for sleutel, waarde in ():
+        print(f"{sleutel} : {waarde} euro")
+
+    print("=" * 26)
+    print(f"totaal : {totaal} euro")
+
+
+
+mijn_dict = {'vis': 10, 'vlees': 25, 'overig': 15}
+totaal = 50
+
+presenteer(mijn_dict, totaal)
+
+vis : 10 
+vlees : 25 
+overig : 15 
+totaal : 50 
